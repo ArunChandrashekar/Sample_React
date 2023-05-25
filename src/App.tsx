680d78a34c0224b9071812ae754b52b7111628a0
arun <carun106@gmail.com>
2023-05-25 17:03:11 +0530

    included bootstrap and testing for list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import Header from "./Component/Header";
-import Footer from "./Component/Footer";
-import Content from "./Component/Content";
-import Menu from "./Component/Menu";
+import ListGroup from "./Component/ListGroup";
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Menu></Menu>
-      <Content></Content>
-      <Footer></Footer>
+      <ListGroup></ListGroup>
     </div>
   );
 }
