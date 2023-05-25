6fcd1d1a585414cc543d8c83a42c89a613f3379e
arun <carun106@gmail.com>
2023-05-25 16:33:06 +0530

    removed unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-
-import Message from './Component/Message'
-import Header from './Component/Header'
-import Footer from './Component/Footer'
-import Content from './Component/Content'
-import Menu from './Component/Menu'
+import Header from "./Component/Header";
+import Footer from "./Component/Footer";
+import Content from "./Component/Content";
+import Menu from "./Component/Menu";
 
 function App() {
-
   return (
-
     <div>
       <Header></Header>
       <Menu></Menu>
       <Content></Content>
       <Footer></Footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
