e81cd8babec02eb93cbe1fd3663b5956b1ab87e8
arun <carun106@gmail.com>
2023-05-26 08:02:27 +0530

    assignment: removed unwanted import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Content from "./Component/Content";
 import Footer from "./Component/Footer";
 import Header from "./Component/Header";
-import ListGroup from "./Component/ListGroup";
 
 function App() {
   return (
