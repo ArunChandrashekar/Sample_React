6ec419aaeac8582c4ec25f707d85f168f2a9ee80
arun <carun106@gmail.com>
2023-05-26 15:21:31 +0530

    usage of props

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import Header from "./Component/Header";
 import Menu from "./Component/Menu";
 
 function App() {
+  let menuItems = ["submenu1", "submenu2", "submenu3", "submenu4", "submenu5"];
   return (
     <div>
       <Header></Header>
-      <Menu
-        heading="Hello"
-        items={["submenu1", "submenu2", "submenu3", "submenu4", "submenu5"]}
-      ></Menu>
+      <Menu heading="Hello" items={menuItems}></Menu>
       <Content></Content>
       <Footer></Footer>
     </div>
