f1c23b2cea3094b7b4aa86268eb3f2706f2985d5
arun <carun106@gmail.com>
2023-05-30 12:51:57 +0530

    fetch api using axios and consoling

--- src/Sample/UserList.tsx
+++ src/Sample/UserList.tsx
@@ -1,13 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+interface Users {
+  id: number;
+  name: string;
+}
 
 const UserList = () => {
+  const [users, setUsers] = useState<Users[]>([]);
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
       console.log(res.data);
+      setUsers(res.data);
     });
-  });
-  return <div></div>;
+  }, []);
+  return (
+    <div>
+      <ul>
+        {users.map((x) => (
+          <li key={x.id}>{x.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UserList;
